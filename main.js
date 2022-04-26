1c52103b06e2c33809fc57986030c4314249f390
Marcel Blum <marcel@soundplant.org>
2022-04-26 15:43:53 -0400

    cleanup

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow} = require('electron');
 
 function createWindow () {
   // Create the browser window.
@@ -33,12 +32,7 @@ app.whenReady().then(() => {
 	createWindow();
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed
 app.on('window-all-closed', function () {
   app.quit();
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
